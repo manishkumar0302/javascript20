74b73d14e44b8d2272ec562347bcd7c49faa00b9
Tej <tejpartapsingh91@gmail.com>
2020-02-11 18:57:43 -0500

    Add link and quote

--- tribute-page/src/App.js
+++ tribute-page/src/App.js
@@ -10,6 +10,30 @@ const App = () => {
           <h2 className="h2">I am Johnny Bravo, the one-man army!</h2>
         </section>
       </header>
+      <main className="main">
+        <blockquote
+          className="blockquote"
+          cite="https://en.wikipedia.org/wiki/Johnny_Bravo"
+        >
+          <span>
+            "When Johnny Bravo first came out, I don't think a lot of people
+            didn't have high hopes for it, and I think it was really cool that
+            prove exactly what kind of character he was. No one really thought
+            it was going to go anywhere. Not only has it gone somewhere, it's
+            actually still around, it's very iconic now, 15, 16 years later."
+          </span>
+          <cite className="cite">
+            <strong>Writer/Director:</strong>{" "}
+            <a
+              href="https://en.wikipedia.org/wiki/Butch_Hartman"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Butch Hartman
+            </a>
+          </cite>
+        </blockquote>
+      </main>
     </>
   );
 };
