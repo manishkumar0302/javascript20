6eb8c8dbcde2c70e44c8226e519d47d04845e7f8
Tej <tejpartapsingh91@gmail.com>
2020-02-11 18:27:07 -0500

    Add JB pic

---
+++ tribute-page/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <>
+      <header className="header">
+        <section className="text">
+          <h1 className="h1">Hey, Baby!</h1>
+          <h2 className="h2">I am Johnny Bravo, the one-man army!</h2>
+        </section>
+      </header>
+    </>
+  );
+};
+
+export default App;
