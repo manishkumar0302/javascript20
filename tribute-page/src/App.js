31d5b0860892e579b91cb849c20dc0515e65fa39
Tej <tejpartapsingh91@gmail.com>
2020-02-12 14:59:22 -0500

    revise particles

--- tribute-page/src/App.js
+++ tribute-page/src/App.js
@@ -13,129 +13,6 @@ const App = () => {
         </section>
       </header>
       <main className="main">
-        <Particles
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            width: "100%",
-            height: "100%"
-          }}
-          className="parent"
-          canvasClassName="child"
-          params={{
-            particles: {
-              number: {
-                value: 10,
-                density: {
-                  enable: true,
-                  value_area: 1000
-                }
-              },
-              color: {
-                value: "#000000"
-              },
-              shape: {
-                type: "circle",
-                stroke: {
-                  width: 5,
-                  color: "#204572"
-                },
-                polygon: {
-                  nb_sides: 5
-                },
-                image: {
-                  src: "img/github.svg",
-                  width: 100,
-                  height: 100
-                }
-              },
-              opacity: {
-                value: 0.4419296542689612,
-                random: false,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  opacity_min: 0.1,
-                  sync: false
-                }
-              },
-              size: {
-                value: 3,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 40,
-                  size_min: 0.1,
-                  sync: false
-                }
-              },
-              line_linked: {
-                enable: true,
-                distance: 150,
-                color: "#ff0000",
-                opacity: 0.4,
-                width: 1
-              },
-              move: {
-                enable: true,
-                speed: 6,
-                direction: "none",
-                random: true,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 1200
-                }
-              }
-            },
-            interactivity: {
-              detect_on: "canvas",
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: "repulse"
-                },
-                onclick: {
-                  enable: true,
-                  mode: "push"
-                },
-                resize: true
-              },
-              modes: {
-                grab: {
-                  distance: 400,
-                  line_linked: {
-                    opacity: 1
-                  }
-                },
-                bubble: {
-                  distance: 400,
-                  size: 40,
-                  duration: 2,
-                  opacity: 8,
-                  speed: 3
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4
-                },
-                push: {
-                  particles_nb: 4
-                },
-                remove: {
-                  particles_nb: 2
-                }
-              }
-            },
-            retina_detect: true
-          }}
-        />
         <blockquote
           className="blockquote"
           cite="https://en.wikipedia.org/wiki/Johnny_Bravo"
@@ -490,6 +367,129 @@ const App = () => {
         </p>
       </main>
       <footer className="footer">
+      <Particles
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            width: "100%",
+            height: "100%"
+          }}
+          className="parent"
+          canvasClassName="child"
+          params={{
+            particles: {
+              number: {
+                value: 80,
+                density: {
+                  enable: true,
+                  value_area: 300
+                }
+              },
+              color: {
+                value: "#000000"
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 5,
+                  color: "#204572"
+                },
+                polygon: {
+                  nb_sides: 5
+                },
+                image: {
+                  src: "img/github.svg",
+                  width: 100,
+                  height: 100
+                }
+              },
+              opacity: {
+                value: 0.4419296542689612,
+                random: false,
+                anim: {
+                  enable: false,
+                  speed: 1,
+                  opacity_min: 0.1,
+                  sync: false
+                }
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 40,
+                  size_min: 0.1,
+                  sync: false
+                }
+              },
+              line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#ff0000",
+                opacity: 0.4,
+                width: 1
+              },
+              move: {
+                enable: true,
+                speed: 6,
+                direction: "none",
+                random: true,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200
+                }
+              }
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse"
+                },
+                onclick: {
+                  enable: true,
+                  mode: "push"
+                },
+                resize: true
+              },
+              modes: {
+                grab: {
+                  distance: 400,
+                  line_linked: {
+                    opacity: 1
+                  }
+                },
+                bubble: {
+                  distance: 400,
+                  size: 40,
+                  duration: 2,
+                  opacity: 8,
+                  speed: 3
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4
+                },
+                push: {
+                  particles_nb: 4
+                },
+                remove: {
+                  particles_nb: 2
+                }
+              }
+            },
+            retina_detect: true
+          }}
+        />
         <span className="message">Love Live Johnny Bravo!</span>
         <span className="author">
           Weaved with <span className="heartbeat">❤</span> by&nbsp;
