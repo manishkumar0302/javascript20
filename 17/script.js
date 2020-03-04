5d6e6d6489f12daf6290c9d62d338decd5c599eb
Tej <tejpartapsingh91@gmail.com>
2020-03-04 15:54:13 -0500

    revise code

--- 17/script.js
+++ 17/script.js
@@ -10,31 +10,25 @@ const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
-// Set background year
-year.innerText = currentYear + 1;
+year.textContent = currentYear + 1;
 
-// Update countdown time
-function updateCountdown() {
+const updateCountdown = () => {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
-
   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
   const m = Math.floor(diff / 1000 / 60) % 60;
   const s = Math.floor(diff / 1000) % 60;
 
-  // Add values to DOM
-  days.innerHTML = d;
-  hours.innerHTML = h < 10 ? "0" + h : h;
-  minutes.innerHTML = m < 10 ? "0" + m : m;
-  seconds.innerHTML = s < 10 ? "0" + s : s;
+  days.textContent = d;
+  hours.textContent = h < 10 ? "0" + h : h;
+  minutes.textContent = m < 10 ? "0" + m : m;
+  seconds.textContent = s < 10 ? "0" + s : s;
 }
 
-// Show spinner before countdown
 setTimeout(() => {
   loading.remove();
   countdown.style.display = "flex";
 }, 1000);
 
-// Run every second
 setInterval(updateCountdown, 1000);
