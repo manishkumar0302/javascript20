777a61e194eaf337bd403659399e502cd0c4d664
Tej <tejpartapsingh91@gmail.com>
2020-03-04 09:19:36 -0500

    add infinite scroll

--- 11/script.js
+++ 11/script.js
@@ -2,24 +2,37 @@ const postsContainer = document.getElementById("posts");
 const loading = document.querySelector(".loading");
 const filter = document.getElementById("filter");
 let limit = 5;
-let page = 1;
+let counter;
+localStorage.getItem("page") ? false : localStorage.setItem("page", JSON.stringify(1));
+localStorage.getItem("data") ? false : localStorage.setItem("data", JSON.stringify([]));
+let currentPage = localStorage.getItem("page");
+let currentData = localStorage.getItem("data");
 
-const getPosts = async () => {
+const getPosts = async (currentPage) => {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${currentPage}`
   );
   const data = await res.json();
-  return data;
+  const cache = [...JSON.parse(currentData), ...data]
+  localStorage.setItem("data", JSON.stringify(cache));
+  currentData = localStorage.getItem("data");
+  return JSON.parse(currentData);
 };
 
-const showPosts = async () => {
-  const posts = await getPosts();
+const showPosts = async (currentPage) => {
+  let totalPosts = JSON.parse(currentData);
+  if(currentPage === totalPosts.length / limit) {
+    posts = totalPosts;
+  } else {
+    posts = await getPosts(currentPage);
+  }
+  postsContainer.textContent = ``;
   posts.forEach(post => {
     const postEl = document.createElement("div");
     postEl.classList.add("post");
     postEl.innerHTML = `
-      <div class="number">${post.id}</div>
-      <div class="post-info">
+    <div class="number">${post.id}</div>
+    <div class="post-info">
         <h2 class="post-title">${post.title}</h2>
         <p class="post-body">${post.body}</p>
       </div>
@@ -30,11 +43,14 @@ const showPosts = async () => {
 
 const showLoading = () => {
   loading.classList.add('show');
+  currentPage = localStorage.getItem("page");
+  counter = JSON.parse(currentPage);
+  counter++;
+  localStorage.setItem("page", JSON.stringify(counter));
   setTimeout(() => {
     loading.classList.remove('show');
     setTimeout(() => {
-      page++;
-      showPosts();
+      showPosts(counter);
     }, 300);
   }, 1000);
 };
@@ -55,11 +71,11 @@ const filterPosts = e => {
 
 window.addEventListener("scroll", () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  if (scrollTop + clientHeight >= scrollHeight) {
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
     showLoading();
   }
 });
 
 filter.addEventListener("input", filterPosts);
 
-showPosts();
+showPosts(JSON.parse(currentPage));
