1be3b2718a4cf6a332c73f5b05f1b74f50399343
Kumar, Manish(mk803m) <mk803m@att.com>
2020-06-06 12:26:59 +0530

    Intial commit changed 1 code

--- 1/script.js
+++ 1/script.js
@@ -1,8 +1,13 @@
 const form = document.getElementById("form");
-const username = document.getElementById("username");
+/* const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const confirmPassword = document.getElementById("confirmPassword");
+const confirmPassword = document.getElementById("confirmPassword"); */
+
+const username=document.querySelector("[name='username']");
+const email=document.querySelector("[name='email']");
+const password = document.querySelector("[name='password']");
+const confirmPassword=document.querySelector("[name='confirmPassword']");
 
 const getFieldName = input =>
   input.id.charAt(0).toUpperCase() + input.id.slice(1);
